70451e51e635d7e9dd4c21cc01e249946297769d
RajeshKumar <130393665+Rajesh110216@users.noreply.github.com>
2023-10-02 23:39:26 +0530

    Add files via upload

---
+++ G1810_AFSD_GradedProject3-main/Ticekt-Tracker-Application/Ticekt-Tracker-Application/src/main/java/com/greatlearning/tickettracker/repository/TicketRepository.java
@@ -0,0 +1,22 @@
+package com.greatlearning.tickettracker.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.greatlearning.tickettracker.entity.Ticket;
+
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+
+    @Query(value = "select * from ticket t where t.title like %:keyword% or t.description like %:keyword%", nativeQuery = true)
+    public List<Ticket> getTicketsBykeyword(@Param("keyword") String keyword);
+
+  
+
+
+}
